1cd23890e301f33e9d0ddc9b976497c61df39c7d
tarangbisen <bisentarang@gmail.com>
2022-05-19 22:17:25 +0530

    First commit

---
+++ HouseRantal_managment_system01/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseRantal_managment_system01
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string Password { get; set; }
+
+        public string City { get; set; }
+    }
+}
